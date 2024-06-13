f5778a70a3f6beec0ba1734d1fbc1522f413db3c
Sakib-Atreus <sakibatreus@gmail.com>
2024-06-13 19:10:31 +0600

    initial setup

---
+++ ticket-booking-dashboard-client/src/routes/Routes.jsx
@@ -0,0 +1,12 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Welcome to the ticket booking dashboard!</div>,
+    },
+  ]);
+
+  export default router;
